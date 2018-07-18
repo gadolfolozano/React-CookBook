db56e3c8d32f8956cf050a2fbcd898be62f9fe99
Adolfo Lozano Mendez <gadolfolozano@gmail.com>
2018-07-17 23:33:58 -0500

    add styles for login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
 import '../styles/main.css';
 
 class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.onUsernameChange = this.onUsernameChange.bind(this);
+    this.onPasswordChange = this.onPasswordChange.bind(this);
+    this.onLoginButtonClick = this.onLoginButtonClick.bind(this);
+  }
+
   componentDidMount() {
 
+  }
+
+  componentDidUpdate() {
+    const { token, user, history } = this.props
+    if (token && user) {
+      history.replace('/home')
+    }
   }
 
   onUsernameChange(event) {
@@ -16,66 +34,30 @@ class Login extends Component {
     this.props.passwordChanged(event.target.value);
   }
 
-  onButtonClick(event) {
+  onLoginButtonClick(event) {
     event.preventDefault();
     const { username, password } = this.props;
     this.props.login(username, password);
   }
 
-  renderErrorUsername(){
-    const { usernameError } = this.props
-
-    if(usernameError) {
-      return <p className="errorText">Invalid username</p>
-    }
-  }
-
-  renderErrorPassword(){
-    const { passwordError } = this.props
-
-    if(passwordError) {
-      return <p className="errorText">Invalid password</p>
-    }
-  }
-
-  renderErrorLogin(){
-    const { loginError } = this.props
-
-    if(loginError) {
-      return <p className="errorText">Authentication failed</p>
-    }
-  }
-
-  componentDidUpdate(prevProps, prevState, snapshot){
-    const { token, user, history } = this.props
-    if(token && user) {
-      history.replace('/home')
-    }
-  }
-
   render() {
     return (
       <div className="container" >
-        <TextField
-          label="Username"
-          type="text"
-          placeholder="Enter Username"
-          required
-          value = {this.props.username}
-          disabled={this.props.isFetching}
-          onChange = {this.onUsernameChange.bind(this)} />
-        {this.renderErrorUsername()}
+
+        <FormControl error={this.props.usernameError} disabled={this.props.isFetching} aria-describedby="name-error-text">
+          <InputLabel htmlFor="name-error">Nombre de usuario</InputLabel>
+          <Input id="name-error" value={this.props.username} onChange={this.onUsernameChange} />
+          <FormHelperText id="name-error-text">{this.props.usernameError ? 'nombre de usuario invalido' : ''}</FormHelperText>
+        </FormControl>
+
+        <br />
         <br />
 
-        <TextField
-          label="Password"
-          type="password"
-          placeholder="Enter Password"
-          required
-          value = {this.props.password}
-          disabled={this.props.isFetching}
-          onChange = {this.onPasswordChange.bind(this)}  />
-        {this.renderErrorPassword()}
+        <FormControl error={this.props.passwordError} disabled={this.props.isFetching} aria-describedby="password-error-text">
+          <InputLabel htmlFor="password-error">Contraseña</InputLabel>
+          <Input id="password-error" type="password" value={this.props.password} onChange={this.onPasswordChange} />
+          <FormHelperText id="password-error-text">{this.props.passwordError ? 'contraseña invalida' : ''}</FormHelperText>
+        </FormControl>
 
         <br />
         <br />
@@ -83,14 +65,27 @@ class Login extends Component {
           variant="contained"
           color="primary"
           disabled={this.props.isFetching}
-          onClick = {this.onButtonClick.bind(this)} >
+          onClick={this.onLoginButtonClick}
+        >
           Login
         </Button>
-        {this.renderErrorLogin()}
+        <FormHelperText error={this.props.loginError} id="login-error-text">{this.props.loginError ? 'Error de login' : ''}</FormHelperText>
 
       </div>
     );
   }
 }
+
+Login.propTypes = {
+  username: PropTypes.string.isRequired,
+  isFetching: PropTypes.bool.isRequired,
+  password: PropTypes.string.isRequired,
+  usernameError: PropTypes.bool.isRequired,
+  passwordError: PropTypes.bool.isRequired,
+  loginError: PropTypes.bool.isRequired,
+  usernameChanged: PropTypes.func.isRequired,
+  passwordChanged: PropTypes.func.isRequired,
+  login: PropTypes.func.isRequired,
+};
 
 export { Login };
